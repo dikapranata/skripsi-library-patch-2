6ef428c28a69a55fdf0d302fc6177b9a3466cc61
dikapranata <dikapranata799@gmail.com>
2017-06-21 17:16:11 +0700

    first commit

--- two.sql
+++ two.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2017 at 11:19 AM
+-- Generation Time: Jun 21, 2017 at 11:30 AM
 -- Server version: 10.1.9-MariaDB
 -- PHP Version: 5.6.15
 
